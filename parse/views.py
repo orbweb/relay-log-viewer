7eb5634c1db90a602b87fb5472e348215949724f
Michael Lai <themichaellai@gmail.com>
2013-08-02 22:27:55 +0800

    Styling

--- parse/views.py
+++ parse/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404, render
 
 def show_sessions(request):
     active = True if request.GET.get('active') else False
-    sessions = RelaySession.objects.filter(active=active)
+    sessions = RelaySession.objects.filter(active=active).order_by('start_time')
     return render(request, 'sessions/show.html', {
         'sessions': sessions
     })
