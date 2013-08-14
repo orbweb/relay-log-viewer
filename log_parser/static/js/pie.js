0c1758a0a18bff2be20d2ce5115fdc6756c7947d
Michael Lai <themichaellai@gmail.com>
2013-08-14 15:30:38 +0800

    pie.js cleanup

--- log_parser/static/js/pie.js
+++ log_parser/static/js/pie.js
@@ -13,20 +13,38 @@ var Pie = function(pie_chart_id, pie_data_id, table_columns, data_url, width, he
       .sort(function(d1, d2) { return d2.data - d1.data; })
       .value(function(d) { return d.data; });
 
-
-  var data = [];
-
-  var svg = d3.select(pie_chart_id).append('svg')
+  var chart = d3.select(pie_chart_id).append('svg')
       .attr('width', width)
       .attr('height', height)
       .append('g')
       .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
   var _that = this;
+
+  var getData = function(querystring, callback) {
+    var querystring = querystring || '';
+    d3.json(data_url + '?' + querystring, function(error, ret_data) {
+      var data = ret_data;
+      data.forEach(function(d) {
+        d.data = +d.data;
+        // comma separate really long numbers
+        d.human_data = Math.floor(d.data)
+          .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      });
+      if (callback) {
+        callback(data);
+      }
+    });
+  };
+
   this.draw = function (querystring) {
-    _that.getData(querystring, function(data) {
+    getData(querystring, function(data) {
       var pie_data = d3.select(pie_data_id);
+
+      // helper methods for highlighting the table rows and changing the
+      // colors of the paths
       var highlightElement = function(d) {
-        var slice = svg.select(pie_chart_id + ' #' + d.category);
+        var slice = chart.select(pie_chart_id + ' #' + d.category);
         var color = d3.rgb(slice.style('fill'));
         slice.style('fill', color.darker(0.35))
           .attr('d', arc.outerRadius(radius));
@@ -34,9 +52,8 @@ var Pie = function(pie_chart_id, pie_data_id, table_columns, data_url, width, he
         var text = pie_data.selectAll(pie_data_id + ' #' + d.category)
           .style('background', '#F5F5F5');
       };
-
       var unhighlightElement = function(d) {
-        var slice = svg.select(pie_chart_id + ' #' + d.category);
+        var slice = chart.select(pie_chart_id + ' #' + d.category);
         var color = d3.rgb(slice.style('fill'));
         slice.style('fill', color.brighter(0.35))
           .attr('d', arc.outerRadius(radius-10));
@@ -45,13 +62,8 @@ var Pie = function(pie_chart_id, pie_data_id, table_columns, data_url, width, he
           .style('background', '#FFF');
       };
 
-      var g = svg.selectAll('.arc')
-          .data(pie(data), function(d) { return d.data.category + d.data.data; });
-      g.enter().append('g')
-          .attr('class', 'arc');
-      g.exit().remove();
-
-      var path = svg.selectAll('path')
+      // handle pie chart
+      var path = chart.selectAll('path')
         .data(pie(data), function(d) { return d.data.category + d.data.data; });
       path.enter().append('path')
           .attr('d', arc)
@@ -61,6 +73,7 @@ var Pie = function(pie_chart_id, pie_data_id, table_columns, data_url, width, he
           .on('mouseout', function(d) { unhighlightElement(d.data); });
       path.exit().remove();
 
+      // handle table
       var rows = pie_data.select('tbody').selectAll('tr')
         .data(data, function(d) { return d.category + d.data; });
       rows.enter().append('tr');
@@ -79,20 +92,6 @@ var Pie = function(pie_chart_id, pie_data_id, table_columns, data_url, width, he
         })
         .enter().append('td')
         .text(function(d) { return d.value; });
-    });
-  };
-  this.getData = function(querystring, callback) {
-    var querystring = querystring || '';
-    d3.json(data_url + '?' + querystring, function(error, ret_data) {
-      var data = ret_data;
-      data.forEach(function(d) {
-        d.data = +d.data;
-        d.human_data = Math.floor(d.data)
-          .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      });
-      if (callback) {
-        callback(data);
-      }
     });
   };
 
