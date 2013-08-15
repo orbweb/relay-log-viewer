19039ffb543cdb4055a7d39d7b5bc719c601d7d6
Michael Lai <themichaellai@gmail.com>
2013-08-15 12:10:44 +0800

    Default route to charts

--- log_parser/static/js/charts.js
+++ log_parser/static/js/charts.js
@@ -11,21 +11,21 @@ var data_columns = [
   {col_name: 'category', display_name: 'UID'},
   {col_name: 'human_data', display_name: 'Data (bytes)'}
 ];
-var data_usage = new Pie('#uid-data-chart', '#uid-data-data', data_columns, 'uid_data.json');
+var data_usage = new Pie('#uid-data-chart', '#uid-data-data', data_columns, '/view/charts/uid_data.json');
 data_usage.initialize();
 
 var data_columns = [
   {col_name: 'category', display_name: 'End Reason'},
   {col_name: 'human_data', display_name: 'Number'}
 ];
-var end_reasons = new Pie('#end-reasons-chart', '#end-reasons-data', data_columns, 'end_reasons.json', 300, 300);
+var end_reasons = new Pie('#end-reasons-chart', '#end-reasons-data', data_columns, '/view/charts/end_reasons.json', 300, 300);
 end_reasons.initialize();
 
 var data_columns = [
   {col_name: 'category', display_name: 'Connection Type'},
   {col_name: 'human_data', display_name: 'Number'}
 ];
-var relay_p2p_counts = new Pie('#relay-p2p-chart', '#relay-p2p-data', data_columns, 'relay_p2p_counts.json', 300, 300);
+var relay_p2p_counts = new Pie('#relay-p2p-chart', '#relay-p2p-data', data_columns, '/view/charts/relay_p2p_counts.json', 300, 300);
 relay_p2p_counts.initialize();
 $('select#month').change(function(obj) {
   relay_p2p_counts.draw($.param(getVals($(this))));
--- log_parser/urls.py
+++ log_parser/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import parse.views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^view/', include('parse.urls')),
+    url(r'^$', parse.views.charts),
 
     url(r'^admin/', include(admin.site.urls)),
 )
